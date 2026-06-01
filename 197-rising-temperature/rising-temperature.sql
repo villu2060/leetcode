a52f41150048e9fcdb949591becdedee01351997
villu2060 <gyawalivilakshan2014@gmail.com>
2026-06-01 10:52:23 +0545

    Time: 307 ms (11.23%) | Memory: 0B (100.00%) - LeetSync

--- 197-rising-temperature/rising-temperature.sql
+++ 197-rising-temperature/rising-temperature.sql
@@ -1,10 +1,6 @@
 -- Write your PostgreSQL query statement below
-WITH Formatted_Weather AS(
-    SELECT id, temperature,
-    recordDate + INTERVAL '1 day' AS extra_date
-    FROM Weather
-)
 
-SELECT w.id FROM 
-Weather w LEFT JOIN Formatted_Weather fw ON w.recordDate=fw.extra_date
-WHERE  w.temperature>fw.temperature;
+SELECT w1.id FROM Weather w1
+LEFT JOIN Weather w2 ON 
+w1.recordDate=w2.recordDate+INTERVAL '1 day'
+WHERE w1.temperature>w2.temperature;
